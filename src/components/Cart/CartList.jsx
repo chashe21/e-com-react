91c6e902246bea5af73deece051ef9db5b253da5
chanoch shechter <chashe21@gmail.com>
2020-05-12 10:57:05 +0300

    initial commit

---
+++ src/components/Cart/CartList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+export default function CartList({ value }) {
+	const { cart } = value;
+
+	return (
+		<div className="container-fluid">
+			{cart.map((item) => {
+				return <CartItem id={item.id} item={item} value={value} />;
+			})}
+		</div>
+	);
+}
